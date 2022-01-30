fe06cf83e9cbcfc797612ef9569af0d9fd83328b
marwinburesch <marwin@posteo.de>
2022-01-30 12:27:48 +0100

    🩹 Rename variables

--- server.js
+++ server.js
@@ -2,30 +2,29 @@ const express = require("express");
 const { readFile } = require("fs/promises");
 
 const app = express();
-
 const xmlJs = require("xml-js");
 
 app.use(express.json());
 
-app.get("/:id", async (req, res) => {
+app.get("/:id", async (request, response) => {
   const xml = await readFile(
-    `./book1/${req.params.id}.xml`,
+    `./book1/${request.params.id}.xml`,
     {
       encoding: "utf-8",
     },
-    (err, data) => {
-      if (err) throw err;
+    (error, data) => {
+      if (error) throw error;
       return data;
     }
   );
 
-  const parsed = xmlJs.xml2json(xml, {
+  const parsedXml = xmlJs.xml2json(xml, {
     compact: false,
     spaces: 4,
     textFn: (string) => string.trim(),
   });
 
-  res.send(parsed);
+  response.send(parsedXml);
 });
 
 app.listen(4000, () => {
